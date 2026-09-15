41b7e31aaaeefa81175fa2e7724904fdeaed6a20
Nelson Georges <georson00@gmail.com>
2026-09-14 20:33:07 -0400

    feat: Add organization details page and service projects integration

--- src/models/organizations.js
+++ src/models/organizations.js
@@ -1,13 +1,40 @@
-import db from './db.js'
+import db from "./db.js";
 
 const getAllOrganizations = async () => {
-    const query = `
-    SELECT organization_id, name, description, contact_email, logo_filename
+  const query = `
+    SELECT
+    organization_id,
+    name,
+    description,
+    contact_email,
+    logo_filename
     FROM public.organization;
     `;
 
-    const result = await db.query(query);
+  const result = await db.query(query);
 
-    return result.rows;
-}
-export { getAllOrganizations };
+  return result.rows;
+};
+
+
+const getOrganizationDetails = async (organizationId) => {
+  const query = `
+    SELECT
+        organization_id,
+        name,
+        description,
+        contact_email,
+        logo_filename
+    FROM public.organization
+    WHERE organization_id = $1;
+    `;
+
+  const queryParams = [organizationId];
+  const result = await db.query(query, queryParams);
+
+  //Return the firs row of the result set, or null if no rows
+  return result.rows.length > 0 ? result.rows[0] : null;
+};
+
+//Export the model functions
+export { getAllOrganizations, getOrganizationDetails };
--- src/models/projects.js
+++ src/models/projects.js
@@ -20,4 +20,26 @@ const getAllProjects = async () => {
     return result.rows;
 }
 
-export { getAllProjects };
+
+const getProjectsByOrganizationId = async (organizationId) => {
+  const query = `
+        SELECT
+          project.project_id,
+          organization_id,
+          project.title,
+          project.description,
+          project.location,
+          project.project_date
+        FROM public.service_project AS project
+        WHERE project.organization_id = $1
+        ORDER BY project.project_date;
+      `;
+
+  const queryParams = [organizationId];
+  const result = await db.query(query, queryParams);
+
+  return result.rows;
+};
+
+// Export the model functions
+export { getAllProjects, getProjectsByOrganizationId };
